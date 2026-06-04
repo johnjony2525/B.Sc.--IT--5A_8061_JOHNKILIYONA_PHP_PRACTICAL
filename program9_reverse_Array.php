fc27ba0d51de22e0da11b2b0550ef913006ec5a0
jo ny <johnjony2525@gmail.com>
2026-06-04 11:22:05 +0530

    Add files via upload

---
+++ program9_reverse_Array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <>h1>Reverse Array</h1>
+    <?php
+    $Names = array("John","Wani","Kiliyona");
+    $Names_re = array_reverse($Names);
+    echo "$Names_re: ";
+    print_r($Names_re);
+    
+    ?>
+</body>
+</html>
