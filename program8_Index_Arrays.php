12aac030d08cef09e4c5ee2e1e55fe1e50aaf391
jo ny <johnjony2525@gmail.com>
2026-06-04 10:56:18 +0530

    Add files via upload

---
+++ program8_Index_Arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays.php</title>
+</head>
+<body>
+    <h1>Types of Arrays in PHP</h1>
+    <?php
+    // Indexed Array
+    $fruits = array("Apple", "Banana", "Orange");
+    echo " At index 0: " . $fruits[0] . "<br>";
+    echo " At index 1: " . $fruits[1] . "<br>";
+    echo " At index 2: " . $fruits[2] . "<br>";
+
+    ?>
+</body>
+</html>
---
+++ program8_Pasive_Array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Passive Array</h1>
+    <?php
+    $colors = array("Red", "Green", "Blue");
+    echo "Passive Array: ";
+    foreach ($colors as $color) {
+        echo $color;
+    }
+    ?>
+</body>
+</html>
