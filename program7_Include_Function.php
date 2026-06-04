4bba6b704c82cc0cf5bb57fbcfc22f9b4ad8931a
jo ny <johnjony2525@gmail.com>
2026-06-04 10:28:03 +0530

    Add files via upload

---
+++ program7_Include_Function.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include function</title>
+</head>
+<body>
+    <h1>Include function</h1>
+    <?php
+    // Using include function to include another PHP file
+    include 'program1.php'; // This will include the content of program1.php
+    include 'program2.php'; // This will include the content of program2.php
+    ?>
+
+   
+
+    
+</body>
+</html>
