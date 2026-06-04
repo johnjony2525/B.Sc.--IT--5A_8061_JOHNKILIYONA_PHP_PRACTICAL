069284f224fbfb610df770bd684ea1d26d9d8038
jo ny <johnjony2525@gmail.com>
2026-06-04 09:55:23 +0530

    Add files via upload

---
+++ program7_Include&Requre_Function.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Include and Require Function in PHP</h1>
+    <?php   
+    // Using include function to include a file
+    include 'program1.php'; // This will include the contents of program1.php
+    // Using require function to include a file
+    require 'program2.php'; // This will include the contents of program2.php
+    ?>
+
+    
+</body>
+</html>
