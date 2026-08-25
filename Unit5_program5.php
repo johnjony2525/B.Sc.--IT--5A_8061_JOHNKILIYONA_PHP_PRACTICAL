e795cca284ce2e351c5191171a56a882137c40ff
jo ny <johnjony2525@gmail.com>
2026-08-25 09:05:11 +0530

    Add files via upload

---
+++ Unit5_program5.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Form</title>
+</head>
+<body>
+
+    <h2>Registration Form</h2>
+
+    <form>
+        Name:
+        <input type="text" name="name" required>
+        <br><br>
+
+        Email:
+        <input type="email" name="email" required>
+        <br><br>
+
+        Password:
+        <input type="password" name="password" required>
+        <br><br>
+
+        Mobile:
+        <input type="text" name="mobile" required>
+        <br><br>
+
+        <input type="submit" value="Register">
+    </form>
+
+</body>
+</html>
